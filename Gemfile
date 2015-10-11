0637231c75a39c75baf5b51933d06342225cea8e
Tom ten Thij <code@tomtenthij.nl>
2015-10-11 22:38:28 +0200

    Search can now look in encrypted messages as well

--- Gemfile
+++ Gemfile
@@ -2,5 +2,5 @@ source "https://rubygems.org"
 ruby "2.2.3"
 
 gem 'sqlite3'
-# gem 'pry-byebug'
-gem 'pry-byebug', '=1.3.3'
+gem 'pry-byebug'
+gem 'gpgme'
--- lib/gpg.rb
+++ lib/gpg.rb
@@ -1,6 +1,28 @@
+require 'gpgme'
+
 class GPG
-  def self.decrypt(content)
-    binding.pry
-    content
+  def self.decrypt(encrypted_content)
+    @@instance ||= GPG.new
+    @@instance.decrypt(encrypted_content)
+  end
+
+  def decrypt(encrypted_content)
+    encrypted_data = GPGME::Data.new(encrypted_content)
+    decrypted = @ctx.decrypt encrypted_data
+    decrypted.seek(0)
+    decrypted.read
+  end
+
+  def passfunc(obj, uid_hint, passphrase_info, prev_was_bad, fd)
+    io = IO.for_fd(fd, 'w')
+    io.puts "PASSPHRASE"
+    io.flush
+  end
+
+  protected
+
+  def initialize
+    @ctx = GPGME::Ctx.new :passphrase_callback => method(:passfunc)
   end
 end
+
